acb462fe351642ff9ae41ffa56994c942916af4a
Kiefer <kiefer136@hotmail.com>
2016-05-04 11:53:12 -0700

    finished v4

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,14 @@
 get '/' do
   erb :index
 end
+
+get '/users' do
+  Users.all.to_json
+end
+
+post '/users' do
+  name = params[:name]
+  email = params[:email]
+
+  user = User.create(name: name, email: email)
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,34 @@
-$(document).ready(function() {
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(function() {
+  $("#userForm").hide();
+  $("#button_load").on('click', function() {
+    $("#userForm").hide();
+    $.getJSON('/users', function(users) {
+      var table = $("#stuff").find('tbody').empty();
+      users.forEach(function(user) {
+        var tr = $("<tr>").addClass('user').appendTo(table);
+        $("<td>").appendTo(tr).text(user.name);
+        $("<td>").appendTo(tr).text(user.email);
+        $("#results").fadeIn('slow');
+      });
+    });
+  });
+})
+  $("#addUser").on('click', function() {
+    $("#results").hide();
+    $("#userForm").fadeIn('slow');
 });
+// $(function() {
+//   $("#button_load").click(function() {
+//     $.ajax({url:"/users", success: function() {
+//       $.get( "/users", function( data ) {
+//         var obj = JSON.parse(data);
+//         console.log(obj);
+//         $.each(obj, function(index, value) {
+//           $("#stuff").append(value.name + value.email);  
+//         })
+//       });
+//     }
+//   })
+// });
+// });
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
